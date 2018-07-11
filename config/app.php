660f6de500aaa7569648e56b21d2b78d133e56f9
minh <minh@scala-com.jp>
2018-07-11 12:01:42 +0900

    Add model generate package

--- config/app.php
+++ config/app.php
@@ -160,6 +160,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Model generator
+         */
+        Reliese\Coders\CodersServiceProvider::class,
+
     ],
 
     /*
