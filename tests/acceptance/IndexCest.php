a819e7e3b933b107f9290bac718c65e5349f86b2
minh <minh@scala-com.jp>
2018-07-11 14:30:18 +0900

    Add laravel5 module

--- tests/acceptance/IndexCest.php
+++ tests/acceptance/IndexCest.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Database\Capsule\Manager as Capsule;
+use App\Models\User;
 
 class IndexCest
 {
@@ -16,6 +16,15 @@ class IndexCest
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
+        // use Eloquent example
+        $user = User::first();
+
+        // use factories example
+        $user = factory(User::class)->make(); // make user object
+        $user = factory(User::class)->create(); // make user object and insert to database
+
+        var_dump($user);
+
         $I->amOnPage('/');
         $I->see('ログイン');
     }
